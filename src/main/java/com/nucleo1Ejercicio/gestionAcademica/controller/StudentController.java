a7a6f95a0acbb6844cdc5b7cc85de3d89fab8896
DavidZomber935 <davidbra12345@gmail.com>
2024-09-16 00:12:34 -0500

    commit secundario

--- src/main/java/com/nucleo1Ejercicio/gestionAcademica/controller/StudentController.java
+++ src/main/java/com/nucleo1Ejercicio/gestionAcademica/controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/students")
+@RequestMapping("/api/students")
 public class StudentController {
 
     @Autowired
